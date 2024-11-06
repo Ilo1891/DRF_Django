84be8d5d03f9b40cbed4a2c2b81bb3557c12b5b8
Кутибашвили Илья <generals22@mail.ru>
2024-11-06 16:10:56 +0300

    25.1 1-2 is done

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
 from django_filters import rest_framework as filters
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer, UserDetailSerializer
@@ -14,6 +15,19 @@ class UserViewSet(ModelViewSet):
         if self.action == "retrieve":
             return UserDetailSerializer
         return self.serializer_class
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = (AllowAny,)
+        else:
+            permission_classes = (IsAuthenticated,)
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        new_user = serializer.save(is_active=True)
+        new_user.set_password(new_user.password)
+        super().perform_create(new_user)
+
 
 class PaymentViewSet(ModelViewSet):
     queryset = Payment.objects.all()
